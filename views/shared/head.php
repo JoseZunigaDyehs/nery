49ae37e886391e16af7c20729f6beee9f585022f
JoseZunigaDyehs <zuniga.joseignacio@gmail.com>
2018-05-16 01:25:28 -0400

    show clientes php

---
+++ views/shared/head.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cartera - Finanzas</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../nery/assets/css/style.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-expand-lg ">
+        <a class="navbar-brand" href="#">Finanzas</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        <div class="collapse navbar-collapse justify-content-between text-white" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../perfil/perfil.html">Perfil
+            <span class="sr-only">(current)</span>
+          </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?id=11">Clientes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../login/login.html">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cartera.html">Cartera</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../historial/historial.html">Historial</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../estadisticas/estadisticas.html">Estadisticas</a>
+                </li>
+
+            </ul>
+            <a class="nav-link" href="#">Logout</a>
+        </div>
+    </nav>
--- views/view.php
+++ views/view.php
@@ -4,8 +4,12 @@ class view {
     private $_parametros = array();
 
     public function render($viewName) {
-        include("views/$viewName.php");
+      include("views/shared/head.php");
+      include("views/shared/script.php");
+      include("views/$viewName.php");
+      include("views/shared/foot.php");
     }
+
 
     public function getParam($nombre) {
         return $this->_parametros[$nombre];
