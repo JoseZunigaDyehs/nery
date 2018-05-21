fa1f85032867704332b45cb58de5a9604e58ac94
JoseZunigaDyehs <zuniga.joseignacio@gmail.com>
2018-05-21 12:28:43 -0400

    descomento login

--- controller/MainController.php
+++ controller/MainController.php
@@ -12,79 +12,77 @@ class MainController {
     public function execute() {
 		
 			
-		// if(isset($_SESSION["estado"])) //viene algo en la session con el nombre estado?
-		// {
-		// 	//echo "ESTOY AUTENTICADO";
-		// 	//echo $_GET['id'];
-			if(isset($_GET['id'])) // La Variable id que llega por GET está definida";
+			if(isset($_SESSION["estado"])) //viene algo en la session con el nombre estado?
 			{
-			$accion = $_GET['id']; //Asignamos el valor de ID que viene por post a la variable $accion
-			//$url = $_SERVER['PHP_SELF'];
-			switch ($accion) { 
-				case 1:
-					$this->_listarPersonas();
-					break;
-				case 2:
-					$idPersona =  $_GET['idPersona'];
-					$this->_eliminarPersona($idPersona);
-					break;
-				case 3:
-					//$this->_view->render("frm_index");	
-					$this->_view->render("frm_bienvenido");	
-					break;
-				case 4:	
-					$this->_view->render("frm_index");	
-					break;
-				case 5:	
-					$this->_view->render("frm_agregaAlumno");	
-					break;
-				case 6:	
-					$this->_agregarAlumno();	
-          break;
-        case 11:
-          $this->_cartera();
-					break;
-				case 12:
-          $this->_perfil();
-					break;
-				case 13:
-          $this->_clientes();
-					break;
-				case 14:
-          $this->_login();
-          break;
-				case 99:
-					$this->_cerrarSession();	
-					break;	
+			// 	//echo "ESTOY AUTENTICADO";
+			// 	//echo $_GET['id'];
+				if(isset($_GET['id'])) {// La Variable id que llega por GET está definida";
+					$accion = $_GET['id']; //Asignamos el valor de ID que viene por post a la variable $accion
+					//$url = $_SERVER['PHP_SELF'];
+					switch ($accion) { 
+						case 1:
+							$this->_listarPersonas();
+							break;
+						case 2:
+							$idPersona =  $_GET['idPersona'];
+							$this->_eliminarPersona($idPersona);
+							break;
+						case 3:
+							$this->_view->render("frm_bienvenido");	
+							break;
+						case 4:	
+							$this->_view->render("frm_index");	
+							break;
+						case 5:	
+							$this->_view->render("frm_agregaAlumno");	
+							break;
+						case 6:	
+							$this->_agregarAlumno();	
+							break;
+						case 11:
+							$this->_cartera();
+							break;
+						case 12:
+							$this->_perfil();
+							break;
+						case 13:
+							$this->_clientes();
+							break;
+						case 14:
+							$this->_login();
+							break;
+						case 99:
+							$this->_cerrarSession();	
+							break;	
+					}
+				}			
+			}
+			else
+			{
+				if(isset($_GET['id']))//viene algo en la varible id del GET?
+				{
+						if ($_GET['id'] == 0) // esa variable es = 0?
+						{
+							if(isset($_POST['login'])) //viene la variable login en el POST?
+							{
+								$this->_validarLogin(); // solo si viene el dato por POST validaré al usuario
+							}
+							else //Esto quiere decir que no se ha logueado por lo tanto lo envío a loguearse
+							{
+								$this->_view->render("frm_login");
+							}
+						}
+						else // como no viene 0, en el id del GET envío a loguear
+						{
+							$this->_view->render("frm_login"); 
+						}
 				}
-			}			
-		// }
-		// else
-		// {
-		// 	if(isset($_GET['id']))//viene algo en la varible id del GET?
-		// 	{
-		// 			if ($_GET['id'] == 0) // esa variable es = 0?
-		// 			{
-		// 				if(isset($_POST['login'])) //viene la variable login en el POST?
-		// 				{
-		// 					$this->_validarLogin(); // solo si viene el dato por POST validaré al usuario
-		// 				}
-		// 				else //Esto quiere decir que no se ha logueado por lo tanto lo envío a loguearse
-		// 				{
-		// 					$this->_view->render("frm_login");
-		// 				}
-		// 			}
-		// 			else // como no viene 0, en el id del GET envío a loguear
-		// 			{
-		// 				$this->_view->render("frm_login"); 
-		// 			}
-		// 	}
-		// 	else // como no viene nada en el id del GET envío a loguear
-		// 	{
-		// 		$this->_view->render("frm_login");
-		// 	}
+				else // como no viene nada en el id del GET envío a loguear
+				{
+					$this->_view->render("frm_login");
+				}
 
-		// }		
+			}		
 
   }
   
@@ -95,14 +93,15 @@ class MainController {
         $this->_view->render("frm_listado");  
 				//var_export($respuesta);
 		
-    }
-	public function _eliminarPersona($idPersona)
-	{
+		}
+		
+	
+		public function _eliminarPersona($idPersona)	{
 		 $control = new control();
 		 $control->eliminarPersona($idPersona);
 	}
-	public function _validarLogin()
-	{
+
+	public function _validarLogin()	{
 		 $control = new control();
 		 $log =  $_POST['login'];
 		 $pass =  $_POST['pass'];
@@ -118,8 +117,8 @@ class MainController {
 		 }
 		 
 	}
-	public function _agregarAlumno()
-	{
+
+	public function _agregarAlumno()	{
 		 $control = new control();
 		 $nombre =  $_POST['name'];
 		 $apellido =  $_POST['lname'];
@@ -127,8 +126,8 @@ class MainController {
 		 $respuesta = $control->insertarAlumno($nombre, $apellido,$sexo);
  
 	}
-	public function _cerrarSession()
-	{
+
+	public function _cerrarSession()	{
 		@session_start();
 		session_destroy();
 		header("Location: index.php");
